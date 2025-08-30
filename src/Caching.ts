05cd77c89e577ef1fda73787c92ffebece5eea16
xllecs <aleexdavidoiu@gmail.com>
2025-08-30 16:17:36 +0300

    add redis to docker compose

--- src/Caching.ts
+++ src/Caching.ts
@@ -1,5 +1,9 @@
 import { createClient } from "redis"
 
-export const redisClient = await createClient()
+// export const redisClient = await createClient()
+//   .on("error", (err) => console.log("Redis Client Error", err))
+//   .connect()
+
+export const redisClient = await createClient({ url: "redis://redis:6379" })
   .on("error", (err) => console.log("Redis Client Error", err))
   .connect()
